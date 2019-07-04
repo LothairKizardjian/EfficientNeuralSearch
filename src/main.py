4a79bec02959b3b1ccbf10c8fd638a7da19da662
kizalo18 <kizalo18@b048-05.crio.dauphine.fr>
2019-07-04 17:20:46 +0200

    trying to fix Exhausted ressource error on GPU, altought it's working fine on CPU

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from data_loader import *
 from model_chess import *
 #from game  import *
 
-nb_games = sys.maxsize
+#nb_games = sys.maxsize
+nb_games = 50
 paths = []
 
 paths.append("../PGN_chess_games/chess_games_20{}.pgn".format('00'))
@@ -15,7 +16,7 @@ tensors,labels = load_data_from_multiple_files(paths,nb_games)
 print(tensors.shape)
 print(labels.shape)
 
-model = model_chess(tensors, labels, batch_size=10, mini_batch_size=1, epoch_nb=50)
+model = model_chess(tensors, labels, batch_size=10, mini_batch_size=1, epoch_nb=20)
 model.train(10)
 
 
