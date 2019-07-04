a0370a8ba4f00e93f5ae30eecd0070c814f8a5d4
Lothair <lothair.kizardjian@laposte.net>
2019-07-04 14:50:10 +0200

    .

--- src/main.py
+++ src/main.py
@@ -19,12 +19,13 @@ nb_games = sys.maxsize
 paths = []
 
 paths.append("../PGN_chess_games/chess_games_20{}.pgn".format('00'))
-paths.append("../PGN_chess_games/chess_games_20{}.pgn".format('01'))
+#paths.append("../PGN_chess_games/chess_games_20{}.pgn".format('01'))
 print('1')
 uci_labels = create_uci_labels()
 print('2')
 tensors,labels = load_data_from_multiple_files(paths,nb_games)
-
+print(tensors.shape)
+print(labels.shape)
 #One_hot encoding
 print('3')
 labels = np.asarray([one_hot_encoded_label(y,uci_labels) for y in labels])
@@ -50,20 +51,20 @@ with tf.device(DEVICE_NAME):
 
 #training
 with  tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-nb_train = 100
-nb_ex    = int(len(tensors)/nb_train)
-for i in range(nb_train):
-    print('Training pool {}/{} ...'.format(i+1,nb_train))
-    x,y = select_random_examples(tensors,labels,nb_ex)
-    print(x.shape)
-    print(y.shape)
-    x_train = x[0:len(x)-int(len(x)/5),:]
-    y_train = y[0:len(y)-int(len(y)/5)]
-
-    x_test = x[len(x)-int(len(x)/5):,:]
-    y_test = y[len(y)-int(len(y)/5):]
-    
-    model.fit(x_train, y_train,batch_size=50, validation_data=(x_test, y_test), epochs=15)
+    nb_train = 100
+    nb_ex    = int(len(tensors)/nb_train)
+    for i in range(nb_train):
+        print('Training pool {}/{} ...'.format(i+1,nb_train))
+        x,y = select_random_examples(tensors,labels,nb_ex)
+        print(x.shape)
+        print(y.shape)
+        x_train = x[0:len(x)-int(len(x)/5),:]
+        y_train = y[0:len(y)-int(len(y)/5)]
+        
+        x_test = x[len(x)-int(len(x)/5):,:]
+        y_test = y[len(y)-int(len(y)/5):]
+        
+        model.fit(x_train, y_train,batch_size=50, validation_data=(x_test, y_test), epochs=15)
 
 model_json = model.to_json()
 with open("./data/models/model.json",'r'):
