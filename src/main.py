db13b9b3068ab3a9beea66a30661a9b4411444dd
Lothair <lothair.kizardjian@laposte.net>
2019-07-04 17:14:10 +0200

    .

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ tensors,labels = load_data_from_multiple_files(paths,nb_games)
 print(tensors.shape)
 print(labels.shape)
 
-model = model_chess(tensors, labels, batch_size=100000, mini_batch_size=25, epoch_nb=50)
-model.train(50)
+model = model_chess(tensors, labels, batch_size=10, mini_batch_size=1, epoch_nb=50)
+model.train(10)
 
 
--- src/model_chess.py
+++ src/model_chess.py
@@ -12,6 +12,11 @@ from pgn_tensors_utils import create_uci_labels
 
 DEVICE_NAME = "/device:GPU:0"
 
+config = tf.ConfigProto(log_device_placement=True)
+config.gpu_options.allow_growth=True
+config.gpu_options.per_process_gpu_memory_fraction = 0.90
+sess = tf.Session(config=config)
+
 class model_chess():
     def __init__(self,
                  tensors,
@@ -20,7 +25,6 @@ class model_chess():
                  mini_batch_size,
                  epoch_nb
     ):
-        print("-" * 20)
         print("Building model ...")
         self.tensors         = tensors
         self.labels          = labels 
@@ -29,20 +33,20 @@ class model_chess():
         self.epoch_nb        = epoch_nb
         self.uci_labels = create_uci_labels()
         
-        with tf.device(DEVICE_NAME):
-            input_boards = Input(shape=(7,8,8))
-            x = Reshape((7,8,8))(input_boards) 
-            h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='same', use_bias=False)(x)))  
-            h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='same', use_bias=False)(h_conv1)))  
-            h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='valid', use_bias=False)(h_conv2))) 
-            h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='valid', use_bias=False)(h_conv3)))
-            h_conv4_flat = Flatten()(h_conv4)       
-            s_fc1 = Dropout(0.3)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024, use_bias=False)(h_conv4_flat))))  
-            s_fc2 = Dropout(0.3)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(s_fc1))))
-            output = Dense(len(create_uci_labels()), activation='softmax', name='output')(s_fc2)  
+        
+        input_boards = Input(shape=(7,8,8))
+        x = Reshape((7,8,8))(input_boards) 
+        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='same', use_bias=False)(x)))  
+        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='same', use_bias=False)(h_conv1)))  
+        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='valid', use_bias=False)(h_conv2))) 
+        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='valid', use_bias=False)(h_conv3)))
+        h_conv4_flat = Flatten()(h_conv4)       
+        s_fc1 = Dropout(0.3)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024, use_bias=False)(h_conv4_flat))))  
+        s_fc2 = Dropout(0.3)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(s_fc1))))
+        output = Dense(len(create_uci_labels()), activation='softmax', name='output')(s_fc2)  
     
-            model = Model(inputs=input_boards, outputs=output)
-            model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+        model = Model(inputs=input_boards, outputs=output)
+        model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
         self.model = model
             
     def sample(self,sample_size,train_size):
@@ -57,14 +61,11 @@ class model_chess():
         return x_train, y_train, x_test, y_test
     
     def train(self,pool_size):
-        config = tf.ConfigProto(log_device_placement=True)
-        config.gpu_options.allow_growth=True
-        with tf.Session(config=config) as sess:
-            print('Training ...')
-            for i in range(pool_size):
-                print('Pool {}/{} :'.format(i,pool_size))
-                x_train,y_train,x_test,y_test = self.sample(self.batch_size,5)
-                self.model.fit(x_train,y_train,batch_size=self.mini_batch_size,validation_data=(x_test,y_test),epochs=self.epoch_nb)
+        print('Training ...')
+        for i in range(pool_size):
+            print('Pool {}/{} :'.format(i,pool_size))
+            x_train,y_train,x_test,y_test = self.sample(self.batch_size,5)
+            self.model.fit(x_train,y_train,batch_size=self.mini_batch_size,validation_data=(x_test,y_test),epochs=self.epoch_nb)
         self.save()
  
     def save(self):
