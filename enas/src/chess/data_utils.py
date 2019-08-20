eb43b9904143b102621b2a3a07ca0c5553cc4418
Lothair <lothair.kizardjian@laposte.net>
2019-08-20 14:39:03 +0200

    failed the commit i guess

---
+++ enas/src/chess/data_utils.py
@@ -0,0 +1,132 @@
+import os
+import sys
+import _pickle as pickle
+import numpy as np
+import tensorflow as tf
+import chess.pgn
+import pgn_tensors_utils
+import bz2
+
+def read_data(data_path, num_valids=5000):
+  print("-" * 80)
+  print("Reading data")
+
+  nb_games = 50
+  #nb_games = sys.maxsize
+  boards, labels, results = {}, {}, {}
+
+  train_files = [
+    "pgn_games/chess_games_2000.pgn",
+    "pgn_games/chess_games_2001.pgn"
+  ]
+  test_file = [    
+    "pgn_games/chess_games_2002.pgn"
+  ]
+  boards["train"], labels["train"], results["train"] = load_data(data_path, train_files, nb_games)
+  
+  if num_valids:
+    boards["valid"] = boards["train"][-num_valids:]
+    labels["valid"] = labels["train"][-num_valids:]
+    results["valid"]= results["train"][-num_valids:]
+
+    boards["train"] = boards["train"][:-num_valids]
+    labels["train"] = labels["train"][:-num_valids]
+    results["train"]= results["train"][:-num_valids]
+  else:
+    boards["valid"], labels["valid"], results["valid"] = None, None, None
+
+  boards["test"], labels["test"], results["test"] = load_data(data_path, test_file, nb_games)
+  return boards, results
+
+def load_pgn_from_bz2(bz2_path):
+  bz2_file_exist = os.path.isfile(bz2_path)
+  if bz2_file_exist == False:
+    print('File {} not found'.format(bz2_path))
+    return 0
+  with open(bz2_path, 'rb') as source, open(bz2_path.replace('.bz2',''), 'wb') as dest:
+    dest.write(bz2.decompress(source.read()))
+  return 1
+  
+def load_games_from_pgn_path(pgn_path,game_nb):
+  pgn_file_exist = os.path.isfile(pgn_path)
+  if pgn_file_exist == False:
+    if(load_pgn_from_bz2(pgn_path+'.bz2')==0):  
+      print('File {} not found'.format(pgn_path))
+      return 0
+  pgn = open(pgn_path)
+  return load_games_from_pgn(pgn,game_nb)
+
+def load_games_from_pgn(pgn,game_nb):
+  name = pgn.name.split('/')[3].replace('.pgn','')
+  print('Loading games for pgn {} ...'.format(name))
+  games = []
+  game  = chess.pgn.read_game(pgn)
+  counter = 0
+  
+  while game != None and counter < game_nb:
+    games.append(game)
+    game = chess.pgn.read_game(pgn)
+    counter+=1
+      
+  print("{} games loaded".format(counter))
+  return games
+  
+
+def _load_data(data_path,pgn_path,game_nb):
+  '''
+    Load the tensors, the according labels and store them in a .npy file
+    '''
+  suffixe = pgn_path.split('/')    
+  suffixe = suffixe[1].replace('.pgn','')
+  tensors_file_exist = os.path.isfile(data_path+'/tensors/tensors_numpy_{}games_{}.npy'.format(game_nb,suffixe))
+  labels_files_exist = os.path.isfile(data_path+'/tensors/labels_numpy_{}games_{}.npy'.format(game_nb,suffixe))
+
+    
+  print("Loading data for {}".format(suffixe))
+    
+  if tensors_file_exist == False or labels_files_exist == False:      
+      full_name = os.path.join(data_path,pgn_path)
+      games = load_games_from_pgn_path(full_name,game_nb)
+      print("loading tensors and according labels ...")
+      tensors,labels,results = pgn_tensors_utils.tensors_labels_from_games(games)
+      np.save(data_path+'/tensors/labels_numpy_{}games_{}.npy'.format(game_nb,suffixe), labels)
+      np.save(data_path+'/tensors/tensors_numpy_{}games_{}.npy'.format(game_nb,suffixe), tensors)
+      np.save(data_path+'/tensors/results_numpy_{}games_{}.npy'.format(game_nb,suffixe), results)
+  else:
+      tensors = np.load(data_path+'/tensors/tensors_numpy_{}games_{}.npy'.format(game_nb,suffixe))
+      labels  = np.load(data_path+'/tensors/labels_numpy_{}games_{}.npy'.format(game_nb,suffixe))      
+      results = np.load(data_path+'/tensors/results_numpy_{}games_{}.npy'.format(game_nb,suffixe))
+  return tensors,labels,results
+
+def load_data(data_path,paths,game_nb):
+  print("Loading data ...")
+  tensors = []
+  labels  = []
+  results = []
+  for pgn_path in paths:
+      t,l,r = _load_data(data_path,pgn_path,game_nb)
+      for row in t:
+        tensors.append(row)
+      for row in l:
+        labels.append(row)
+      for row in r:
+        results.append(row)
+        
+  tensors = np.asarray(tensors)
+  labels  = np.asarray(labels)
+  results = np.asarray(results)
+  
+  tensors = np.concatenate(tensors, axis=0)
+  tensors = np.reshape(tensors, [-1, 7, 8, 8])
+  tensors = np.transpose(tensors, [0, 2, 3, 1])
+  
+  return tensors,labels,results
+
+def select_random_examples(tensors,labels,nb_ex):
+    if len(tensors) != len(labels) :
+        raise('Tensors and labels have different length')
+    else:
+      samples = np.random.choice(len(tensors), size=nb_ex, replace=False)
+      x = tensors[samples]
+      y = labels[samples]
+    return x,y
---
+++ enas/src/chess/pgn_tensors_utils.py
@@ -0,0 +1,164 @@
+import numpy as np
+import chess.pgn
+import copy
+
+def fen_to_tensor(fen):    
+    pieces_str = "PRNBQK"
+    pieces_str += pieces_str.lower()
+    pieces = set(pieces_str)
+    valid_spaces = set(range(1,9))
+    pieces_dict = {pieces_str[0]:1, pieces_str[1]:2, pieces_str[2]:3, pieces_str[3]:4,
+                    pieces_str[4]:5, pieces_str[5]:6,
+                    pieces_str[6]:-1, pieces_str[7]:-2, pieces_str[8]:-3, pieces_str[9]:-4, 
+                    pieces_str[10]:-5, pieces_str[11]:-6}
+    #tensor = np.zeros((7,8,8)).astype('int32')
+    tensor = np.zeros((7,8,8)).astype('float32')
+    
+    inputliste = fen.split(' ')
+    row = 0
+    col = 0
+    for i, c in enumerate(inputliste[0]):
+        if c in pieces:
+            tensor[np.abs(pieces_dict[c])-1,row, col] = np.sign(pieces_dict[c])
+            col = col + 1
+        elif c == '/':  # new row
+            row = row + 1
+            col = 0
+        elif int(c) in valid_spaces:
+            col = col + int(c)
+        else:
+            raise ValueError("invalid fenstr at index: {} char: {}".format(i, c))
+
+    if inputliste[1] == "w":
+        for i in range(8):
+            for j in range(8):
+                tensor[6,i,j] = 1
+    else:
+        for i in range(8):
+            for j in range(8):
+                tensor[6,i,j] = -1
+  
+    return tensor
+
+def tensors_for_each_move(game):
+    tensors = []
+    board   = game.board()
+    fen     = board.fen()
+    tensors.append(fen_to_tensor(fen))
+    for move in game.mainline_moves():
+        board.push(move)
+        fen = board.fen()
+        tensors.append(fen_to_tensor(fen))
+    return np.asarray(tensors)
+
+def get_result_logits(game):
+    result = game.headers["Result"]
+    if result == "1-0":
+        return np.array([1,0,0])
+    elif result == "0-1":
+        return np.array([0,1,0])
+    else:
+        return np.array([0,0,1])
+    
+def get_result(game):
+    result = game.headers["Result"]
+    if result == "1-0":
+        return 0
+    elif result == "0-1":
+        return 1
+    else:
+        return 2
+    
+def tensors_labels_for_each_move(game):
+    #returns a tensor corresponding to the board's actual state and the move done according to
+    #this state, except for the last one since there's no more moves.
+
+    tensors = []
+    labels  = []
+    results = []
+    board   = game.board()
+    fen     = board.fen()
+    uci     = create_uci_labels()
+
+    for move in game.mainline_moves():
+        results.append(get_result(game))
+        tensors.append(fen_to_tensor(fen))
+        index = uci.index(move.uci())
+        labels.append(index)
+        board.push(move)
+        fen = board.fen()
+
+    return np.asarray(tensors), np.asarray(labels), np.asarray(results)
+
+def tensors_labels_from_games(games):
+    tensors = []
+    labels  = []
+    results = []
+    for game in games:
+        tens, labs, res = tensors_labels_for_each_move(game)
+        if len(tens)==len(labs):
+            for i in range(len(tens)):
+                tensors.append(tens[i])
+                labels.append(labs[i])
+                results.append(res[i])
+    return np.asarray(tensors),np.asarray(labels).astype('int32'),np.asarray(results).astype('int32')
+
+def create_uci_labels():
+    """
+    Creates the labels for the universal chess interface into an array and returns them
+    This returns all the possible 'Queen moves' and 'Knight move' for each square plus the promotion
+    of a pawn to either a rook, knight, bishop or queen from rank 7 or higher
+    :return:
+    """
+    labels_array = []
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8']
+    promoted_to = ['q', 'r', 'b', 'n']
+
+    for l1 in range(8):
+        for n1 in range(8):
+            destinations = [(t, n1) for t in range(8)] + \
+                           [(l1, t) for t in range(8)] + \
+                           [(l1 + t, n1 + t) for t in range(-7, 8)] + \
+                           [(l1 + t, n1 - t) for t in range(-7, 8)] + \
+                           [(l1 + a, n1 + b) for (a, b) in
+                            [(-2, -1), (-1, -2), (-2, 1), (1, -2), (2, -1), (-1, 2), (2, 1), (1, 2)]]
+            for (l2, n2) in destinations:
+                if (l1, n1) != (l2, n2) and l2 in range(8) and n2 in range(8):
+                    move = letters[l1] + numbers[n1] + letters[l2] + numbers[n2]
+                    labels_array.append(move)
+    for l1 in range(8):
+        l = letters[l1]
+        for p in promoted_to:
+            labels_array.append(l + '2' + l + '1' + p)
+            labels_array.append(l + '7' + l + '8' + p)
+            if l1 > 0:
+                l_l = letters[l1 - 1]
+                labels_array.append(l + '2' + l_l + '1' + p)
+                labels_array.append(l + '7' + l_l + '8' + p)
+            if l1 < 7:
+                l_r = letters[l1 + 1]
+                labels_array.append(l + '2' + l_r + '1' + p)
+                labels_array.append(l + '7' + l_r + '8' + p)
+    return labels_array
+    
+def flipped_uci_labels():
+    """
+    Seems to somehow transform the labels used for describing the universal chess interface format, putting
+    them into a returned list.
+    :return:
+    """
+    def repl(x):
+        return "".join([(str(9 - int(a)) if a.isdigit() else a) for a in x])
+
+    return [repl(x) for x in create_uci_labels()]
+
+def one_hot_encoded_label(uci_label,uci_labels):
+    label = np.zeros(len(uci_labels))
+
+    for i in range(len(uci_labels)):
+        if uci_label == uci_labels[i]:
+            label[i] = 1
+            break
+    return label
+        
